22d1ce1247cf7208787943136f2a61b80f07176e
CODE GEASS <92590831+CodeG3ass@users.noreply.github.com>
2025-03-08 00:26:00 +0300

    init

---
+++ helpers/helpers.go
@@ -0,0 +1,35 @@
+package helpers
+
+import (
+	"net/url"
+	"os"
+	"strings"
+)
+
+func RemoveDomainError(url string) bool {
+	if url == os.Getenv("DOMAIN") {
+		return false
+	}
+
+	newURL := strings.Replace(url, "http://", "", 1)
+	newURL = strings.Replace(newURL, "https://", "", 1)
+	newURL = strings.Replace(newURL, "www.", "", 1)
+	newURL = strings.Split(newURL, "/")[0]
+
+	return newURL != os.Getenv("DOMAIN")
+}
+
+func EnforceHTTP(url string) string {
+	if url[:4] != "http" {
+		return "http://" + url
+	}
+	return url
+}
+
+func IsValidURL(urlString string) bool {
+	parseUrl, err := url.Parse(urlString)
+	if err != nil {
+		return false
+	}
+	return parseUrl.Scheme == "" || parseUrl.Scheme == "http" || parseUrl.Scheme == "https"
+}
